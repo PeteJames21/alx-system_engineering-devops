13e1784d376e541080e8da445a1acf259afedd43
PeteJames21 <peetjames21@gmail.com>
2023-10-03 22:00:24 +0300

    Match 1 or more uppercase chars

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-pattern = /[A-Z]*/
+pattern = /[A-Z]+/
 str = ARGV[0]
 matches = str.scan(pattern)  # Find matches
 puts matches.join()  # Concatenate the matching strings and print them
