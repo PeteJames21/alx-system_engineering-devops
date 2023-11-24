614ea35b330ef5e1221b90d49a497dfc4a1ff77b
PeteJames21 <peetjames21@gmail.com>
2023-11-24 23:13:25 +0300

    Refactor

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,5 @@
 # Install flask using pip3
-
-package { 'python3':
-  ensure => present
-}
-
-class { 'python':
-  version => 'system'
-}
-
-package { 'python3-pip':
-    ensure  => present
-}
-
-package { 'flask':
+package { 'flask' :
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip']
 }
