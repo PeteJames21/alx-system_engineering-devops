eb1942316757da5193bd9b38400aa03e5c75d977
PeteJames21 <peetjames21@gmail.com>
2023-11-24 23:12:40 +0300

    Remove trailing commas

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,19 @@
 # Install flask using pip3
 
 package { 'python3':
-  ensure => present,
+  ensure => present
 }
 
 class { 'python':
-  version => 'system',
+  version => 'system'
 }
 
 package { 'python3-pip':
-    ensure  => present,
+    ensure  => present
 }
 
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip'],
+    require  => Package['python3-pip']
 }
